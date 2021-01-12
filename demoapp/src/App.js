3d176e0edc2e0c72b47306767dd8dfb73a0805a9
Tarun Nagpal <tarun.nagpal78@gmail.com>
2021-01-12 14:22:08 +0530

    Added Greetings component

--- demoapp/src/App.js
+++ demoapp/src/App.js
@@ -1,20 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Greetings from './Greetings';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greetings/>
     </div>
   );
 }
---
+++ demoapp/src/Greetings.js
@@ -0,0 +1,4 @@
+
+const Greetings = () => (<h1>hello world</h1>);
+
+export default Greetings;
