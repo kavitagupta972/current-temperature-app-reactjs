7afb3f24eb39711d671737e2c2b73becd905a384
Tarun Nagpal <tarun.nagpal78@gmail.com>
2021-01-12 16:43:31 +0530

    added form

--- demoapp/src/WeatherForecast/WeatherForecast.js
+++ demoapp/src/WeatherForecast/WeatherForecast.js
@@ -2,13 +2,24 @@ import { useState } from "react";
 const WeatherForecast = () =>{
 
     let [city, setCity] =useState(" ");
+    let [unit, setUnit] = useState('imperial')
 
     function onCityChange(event){
         setCity(event.target.value);
         console.log(city);
     }
+
+    function getWeatherForecast() {
+        //TODO
+    }
+
+    function onUnitChange(event){
+        setUnit(event.target.value);
+    }
     return (
-        <div className = "weather-forecast-div">
+        <div className = "weather-forecast-form-div">
+            <h2>Find Current Temperature</h2>
+            <form id = "weather-form" onSubmit = {getWeatherForecast}>
             <label>Enter City</label>
             <input type = "text"
             name = "city"
@@ -18,6 +29,27 @@ const WeatherForecast = () =>{
             value = {city}
             onChange = {onCityChange}
             ></input>
+            <br/>
+            <br/>
+            <label>
+                <input type = "radio"
+                name="units"
+                value="imperial"
+                checked = {unit === 'imperial'}
+                onChange={onUnitChange}/>
+                Farneheit
+            </label>
+            <label>
+                <input type = "radio"
+                name = "units"
+                value="metric"
+                checked = {unit === 'metric'}
+                onChange={onUnitChange}/>
+                Celcius
+            </label>
+            <br/>
+            <input type="submit" value ="submit" className="submit-button"/>
+            </form>
         </div>
     )
 }
